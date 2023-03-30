e894e27d65f95972071e93c0e0c9da0198749edc
andypsh <andypsh@naver.com>
2023-03-30 13:25:47 +0900

    03/30 from Labtop

--- 06_심화_1/너의 평점은.py
+++ 06_심화_1/너의 평점은.py
@@ -2,7 +2,6 @@ import sys
 
 score = ['A+' , 'A0' ,'B+' , 'B0' , 'C+','C0','D+','D0','F']
 pyeong = [4.5 , 4.0 , 3.5 , 3.0 , 2.5 , 2.0 , 1.5 , 1.0 , 0.0]
-
 isu_hak = 0
 hak = 0
 for i in range(20):
@@ -10,7 +9,4 @@ for i in range(20):
     if A[2]!='P':
         hak+= pyeong[score.index(A[2])] * float(A[1])
         isu_hak += float(A[1])
-
 print(hak/ (isu_hak))
-
-#%%
